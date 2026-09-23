a369b39c56fd1b4aca1aee61571051f1b6557238
Raymond Beehler <21GunSoftware@comcast.net>
2026-09-22 21:06:32 -0700

    Remove  unneccesary using statement..

--- Mod Source/Parallax/Harmony Patches/PQSStartPatch.cs
+++ Mod Source/Parallax/Harmony Patches/PQSStartPatch.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using static Parallax.Legacy.LegacyScatterConfigLoader;
 using static Parallax.PQSStartPatch;
 
 namespace Parallax
